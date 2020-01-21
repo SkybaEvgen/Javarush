4dfd5ca7581f0f451570cfa740171a59045c8726
Evgen <evgen.skyba@gmail.com>
2020-01-21 15:03:23 +0100

    6

---
+++ Level_17_Multithreading/6/src/Given/OurPresident.java
@@ -0,0 +1,12 @@
+package Given;
+
+public class OurPresident {
+    private static OurPresident president;
+
+    private OurPresident() {
+    }
+
+    public static OurPresident getOurPresident() {
+        return president;
+    }
+}
---
+++ Level_17_Multithreading/6/src/Solution/OurPresident.java
@@ -0,0 +1,18 @@
+package Solution;
+
+public class OurPresident {
+    private static OurPresident president;
+
+    private OurPresident() {
+    }
+
+    public static OurPresident getOurPresident() {
+        return president;
+    }
+
+    static {
+        synchronized (OurPresident.class) {
+            president = new OurPresident();
+        }
+    }
+}
