4f3ca2426b6b23e8f2146dbbf4b787b144a27635
Evgen <evgen.skyba@gmail.com>
2020-01-21 14:32:44 +0100

    5

---
+++ Level_17_Multithreading/5/src/Given.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Сад-огород
+*/
+
+public class Given {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Garden {
+
+        public final List<String> fruits = new ArrayList<String>();
+        public final List<String> vegetables = new ArrayList<String>();
+
+    }
+}
