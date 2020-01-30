1ff956028137b949d5115b55fda5d5cdef2fdfc9
Evgen <evgen.skyba@gmail.com>
2020-01-30 15:10:02 +0100

    10

---
+++ Level_17_Multithreading/10/src/Given.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/* 
+Общий список
+*/
+
+public class Given {
+    private ArrayList<Long> original = new ArrayList<Long>();
+
+    public static void main(String[] args) {
+
+    }
+}
